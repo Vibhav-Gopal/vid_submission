3eca3040b139443d1a9c956649455c93389a88dc
Vibhav Gopal <vibhavgopal2004@gmail.com>
2023-12-18 22:21:29 -0800

    Raghav made me do this, extra motion stuff

--- scripts/demonstrate.py
+++ scripts/demonstrate.py
@@ -26,26 +26,79 @@ bot.resetAllThrusters()
 # raise(NotImplementedError("Target Heavepoint not set, PID Values not set "))
 bot.setTargetHeavePoint(0)
 
-HEAVE_K_P=0.01
-HEAVE_K_I=0.01
-HEAVE_K_D=0.01
-HEAVE_ACCEPTABLE_ERROR=0.1
-bot.setHeavePIDConstants()
+HEAVE_K_P=0.1
+HEAVE_K_I=0.1
+HEAVE_K_D=0.1
+HEAVE_ACCEPTABLE_ERROR=6
+bot.setHeavePIDConstants(HEAVE_K_P,HEAVE_K_I,HEAVE_K_D,HEAVE_ACCEPTABLE_ERROR)
 
 def updateDepth(data):
-    depth = data.z
+    depth = data.position.z
     bot.updateCurrentHeavePoint(depth)
     bot.updateThrustValues()
     bot.refresh()
 
 def movement():
-    sleep(3) # Let bot settle at a depth
-    bot.setSurgeThrust(10)
+    bot.setSurgeThrust(0)
+    bot.setYawThrust(0)
+    bot.setPitchThrust(0)
+    bot.setRollThrust(0)
+    bot.setSwayThrust(0)
     bot.updateThrustValues()
     bot.refresh()
 
-DEPTHTOPIC = "/depth"
-MSGTYPE = String
+    sleep(2) # Let bot settle at a depth
+    bot.setSurgeThrust(10)
+    bot.updateThrustValues()
+    bot.refresh()
+    sleep(2)
+    bot.setSurgeThrust(-10)
+    bot.updateThrustValues()
+    bot.refresh() 
+    sleep(2)
+    bot.setSurgeThrust(0)
+    bot.setSwayThrust(10)
+    bot.updateThrustValues()
+    bot.refresh()
+    sleep(1)
+    bot.setSwayThrust(-10)
+    bot.updateThrustValues()
+    bot.refresh()
+    sleep(1)
+    bot.setSwayThrust(0)
+    bot.setYawThrust(10)
+    bot.updateThrustValues()
+    bot.refresh()
+    sleep(1)
+    bot.setYawThrust(-10)
+    bot.updateThrustValues()
+    bot.refresh()
+    sleep(1)
+    bot.setYawThrust(0)
+    bot.setPitchThrust(10)
+    bot.updateThrustValues()
+    bot.refresh()
+    sleep(1)
+    bot.setPitchThrust(-10)
+    bot.updateThrustValues()
+    bot.refresh()
+    sleep(1)
+    bot.setRollThrust(10)
+    bot.setPitchThrust(0)
+    bot.updateThrustValues()
+    bot.refresh()
+    sleep(1)
+    bot.setRollThrust(-10)
+    bot.updateThrustValues()
+    bot.refresh()
+    sleep(1)
+    bot.setRollThrust(0)
+    bot.updateThrustValues()
+    bot.refresh()
+
+
+DEPTHTOPIC = "/pose"
+MSGTYPE = Pose
 rospy.init_node("Bot")
 rospy.Subscriber(DEPTHTOPIC,MSGTYPE,callback=updateDepth)
 movementThread = threading.Thread(target=movement)
